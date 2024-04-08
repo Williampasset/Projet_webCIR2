e56a019996c810e2076e04a5a79b74baba7859a8
Williampasset <william.passet@student.junia.com>
2024-04-08 21:42:02 +0200

    ajout rotation

--- public/tetris.js
+++ public/tetris.js
@@ -61,6 +61,7 @@ function create() {
     //cursors.down.on('down', fallBlocks, this);
     cursors.left.on('down', moveBlockLeft, this);
     cursors.right.on('down', moveBlockRight, this);
+    this.input.keyboard.on('keydown-R', rotateBlock, this);
 
     // Démarrer le jeu en créant un premier bloc Tetris
     createNewBlock(this);
@@ -122,11 +123,11 @@ function generateRandomBlock() {
 }
 
 //Fonction pour placer un bloc sur la carte
-function placeBlockOnMap(block, mapData) {
+function placeBlockOnMap(block, mapData, type) {
     for (let y = 0; y < block.pattern.length; y++) {
         for (let x = 0; x < block.pattern[y].length; x++) {
             if (block.pattern[y][x] === 1) {
-                mapData[block.y + y][block.x + x] = 2;
+                mapData[block.y + y][block.x + x] = type;
             }
         }
     }
@@ -142,6 +143,7 @@ function clearBlockFromMap(block, mapData) {
         }
     }
 }
+
 // Fonction pour dessiner la carte de jeu
 function drawMap(scene, tileSize, mapWidth, mapHeight, mapData) {
     // Dessiner chaque tuile de la carte
@@ -162,6 +164,9 @@ function drawMap(scene, tileSize, mapWidth, mapHeight, mapData) {
                 case 2:
                     scene.add.image(tileX, tileY, 'violetblock').setOrigin(0.5);
                     break;
+                case 3:
+                    scene.add.image(tileX, tileY, 'violetblock').setOrigin(0.5);
+                    break;
             }
         }
     }
@@ -175,7 +180,7 @@ function fallBlocks(scene) {
     if (canBlockMove(activeBlock, mapData, 'down')) {
         clearBlockFromMap(activeBlock, mapData);
         activeBlock.y++;
-        placeBlockOnMap(activeBlock, mapData);
+        placeBlockOnMap(activeBlock, mapData,2);
         drawMap(scene, blockSize, 10, 18, mapData);
     }
 }
@@ -187,10 +192,12 @@ function fallBlocksAutomatically(scene) {
         console.log('Block can move down');
         clearBlockFromMap(activeBlock, mapData);
         activeBlock.y++;
-        placeBlockOnMap(activeBlock, mapData);
+        placeBlockOnMap(activeBlock, mapData, 2);
         drawMap(scene, blockSize, 10, 18, mapData);
     } else {
+        placeBlockOnMap(activeBlock, mapData, 3);
         timer.remove(false);
+        drawMap(scene, blockSize, 10, 18, mapData);
         createNewBlock(scene);
     }
 }
@@ -221,17 +228,9 @@ function canBlockMove(block, mapData, direction) {
                     console.log('Sortie de la carte');
                     return false;
                 }
-                if (mapData[newY + y][newX + x] === 2) {
-                    // Vérifier s'il y a un bloc violet en dessous
-                    if (mapData[newY + y + 1][newX + x] === 2) {
-                        // Vérifier si le bloc violet appartient au même motif en mouvement
-                        if (newX + x === block.x && newY + y + 1 === block.y) {
-                            return true; // Le déplacement est autorisé
-                        } else {
-                            console.log('Collision avec un bloc existant');
-                            return false; // Collision avec un autre bloc violet
-                        }
-                    }
+                if (mapData[newY + y][newX + x] === 3) {
+                    console.log('Collision avec un bloc Tetris');
+                    return false;
                 }
                         
             }
@@ -246,7 +245,7 @@ function moveBlockLeft() {
     if (canBlockMove(activeBlock, mapData, 'left')) {
         clearBlockFromMap(activeBlock, mapData);
         activeBlock.x--;
-        placeBlockOnMap(activeBlock, mapData);
+        placeBlockOnMap(activeBlock, mapData, 2);
         drawMap(this, blockSize, 10, 18, mapData);
     }
 }
@@ -256,12 +255,49 @@ function moveBlockRight() {
     if (canBlockMove(activeBlock, mapData, 'right')) {
         clearBlockFromMap(activeBlock, mapData);
         activeBlock.x++;
-        placeBlockOnMap(activeBlock, mapData);
+        placeBlockOnMap(activeBlock, mapData, 2);
         drawMap(this, blockSize, 10, 18, mapData);
     }
 }
 
 // Fonction de rotation du bloc
 function rotateBlock() {
-    // TODO
+    const rotatedPattern = [];
+    // Transposer la matrice du bloc
+    for (let x = 0; x < activeBlock.pattern[0].length; x++) {
+        const newRow = activeBlock.pattern.map((row) => row[x]).reverse();
+        rotatedPattern.push(newRow);
+    }
+
+    // Vérifier si la rotation est autorisée
+    if (canRotateBlock(activeBlock, mapData, rotatedPattern)) {
+        clearBlockFromMap(activeBlock, mapData);
+        activeBlock.pattern = rotatedPattern;
+        placeBlockOnMap(activeBlock, mapData, 2);
+        drawMap(this, blockSize, 10, 18, mapData);
+    }
+}
+
+// Fonction pour vérifier si un bloc peut être tourné
+function canRotateBlock(block, mapData, rotatedPattern) {
+    // Vérifier les collisions après la rotation
+    for (let y = 0; y < rotatedPattern.length; y++) {
+        for (let x = 0; x < rotatedPattern[y].length; x++) {
+            if (rotatedPattern[y][x] === 1) {
+                const newX = block.x + x;
+                const newY = block.y + y;
+                
+                // Vérifier si le nouveau placement est valide
+                if (newX < 1 || newX >= mapData[0].length - 1 || newY >= mapData.length - 1) {
+                    console.log('Sortie de la carte');
+                    return false;
+                }
+                if (mapData[newY][newX] === 3 || mapData[newY][newX] === 1) {
+                    console.log('Collision avec un bloc Tetris ou une pierre');
+                    return false;
+                }
+            }
+        }
+    }
+    return true; // Rotation autorisée
 }
